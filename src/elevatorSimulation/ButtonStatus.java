95cfed221173e47770b76c1c9d34a9ba8339f3eb
tharsan <tharsan18@hotmail.ca>
2023-02-04 21:07:57 -0500

    Move Button Status class to its own Enum class

---
+++ src/elevatorSimulation/ButtonStatus.java
@@ -0,0 +1,15 @@
+package elevatorSimulation;
+
+
+/**
+ * Enumerator class to hold an elevator button's possible options
+ * 
+ * @author Group 3, SYSC 3303
+ * @version Milestone 1
+ * @since 2023-02-04
+ *
+ */
+public enum ButtonStatus {
+	Up, 
+	Down
+}
